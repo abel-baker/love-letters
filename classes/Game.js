ca36e0daf98762614773aedc65c7de5de74055d0
abel-baker <seth.keating@gmail.com>
2022-08-30 15:08:06 -0400

    begin convert to players and playerQueue

--- classes/Game.js
+++ classes/Game.js
@@ -26,23 +26,32 @@ class Game {
     this.status = 'inactive';
 
     // map GuildMember -> Player
-    this.players = new Map();
+    // these are the members (and corresponding created Players) who want to play next hand
+    // by default, players in a current hand are left in the queue so they can play next hand automatically
+    this.playerQueue = new Map();
+
+    // Player
+    // these are the Player objects who are currently in (or about to start; or just having left) a hand
+    this.players = new Set();
 
     // set of GuildMember to add/create (map) or remove
+    // get rid of this--members can be queued instantly
     this.queueJoin = new Set();
+    // get rid of this--members can be removed from the queue instantly
     this.queueLeave = new Set();
 
     this.resetCards();
   }
 
 
+  // begin hand
   start() {
     this.status = 'active';
 
     this.setAside();
     console.log(`Setting aside ${this.aside.name}`);
 
-    for (let [member, player] of this.players) {
+    for (let player of this.players) {
       this.deal(player, 1);
     }
   }
@@ -59,7 +68,7 @@ class Game {
     this.deck.shuffle();
 
     // Iterate through Players and clear their hands
-    for (const [member, player] of this.players) {
+    for (let player of this.players) {
       player.clearHand();
     }
 
@@ -77,31 +86,65 @@ class Game {
 
   ///// PLAYER FUNCTIONS /////
 
+  // join the queue (or an open game) to play
   join(member) {
-    if (this.players.has(member)) {
+    // if member is already queued, disallow them being added again (unless debug is active)
+    if (this.playerQueue.has(member)) {
       if (config.debug) {
-        const fakeMember = { ...member, nickname: `fake ${member.nickname}` };
-        this.players.set(fakeMember, new Player(fakeMember));
+        const fakeMember = { ...member, nickname: `fake ${member.nickname} ${this.playerQueue.size}`};
+        this.playerQueue.set(fakeMember, new Player(fakeMember));
         return true;
       }
+
       return false;
     }
 
-    this.players.set(member, new Player(member));
+    // if (this.players.has(member)) {
+    //   if (config.debug) {
+    //     const fakeMember = { ...member, nickname: `fake ${member.nickname}` };
+    //     this.playerQueue.set(fakeMember, new Player(fakeMember));
+    //     this.players.set(fakeMember, new Player(fakeMember));
+    //     return true;
+    //   }
+    //   return false;
+    // }
+
+    // otherwise, proceed to add the new member to player queue
+    // this.players.set(member, new Player(member));
+    this.playerQueue.set(member, new Player(member));
     return true;
   }
-  leave(member) {
-    // If the Game's list of players contains the player in question, remove them.
-    if (this.players.has(member)) {
-      // Add them to the remove queue
-      // this.queueLeave.push(member);
+  leaveQueue(member) {
+    // get some relevant conditions
+    const inGame = this.players.has()
 
-      this.players.delete(member);
-      return true;
-    } else {
-      return false;
+    // remove the member and their Player object from the queue--they won't play next hand
+    // return their Player object for purposes of messaging, scoreboard, etc.
+    if (this.playerQueue.has(member)) {
+      const player = this.playerQueue.get(member);
+      this.playerQueue.delete(member);
+
+      return player;
     }
+
+    return false;
   }
+
+  // removes the specified member from the game, performing necessary logic to resolve losing them.
+  leaveGame(member) {
+    const player = this.MemberIsPlaying(member);
+    if (player) {
+      //TODO
+      // remove from this.players
+      // discard hand?  lay hand facedown?
+      // make a history entry (todo)
+      // make some kind of message
+    }
+
+    return false;
+  }
+
+  /* here */
 
   playing() {
     const playing = Array.from(this.players, ([key, value]) => value);
@@ -125,6 +168,16 @@ class Game {
   isPlaying(query) {
     // Allow either a GuildMember or a Player as query
     return this.players.has(query) || [...this.players.values()].includes(query);
+  }
+  // check this.players for a Player object matching the member argument
+  MemberIsPlaying(member) {
+    for (let player of this.players) {
+      if (member === player.member) {
+        return player;
+      }
+    }
+
+    return false;
   }
 
   currentPlayer() {
